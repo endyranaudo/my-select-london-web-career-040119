8a7f497f546210b03be4608d0ff2567c95544e57
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-21 10:52:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,14 +3,3 @@ def my_select(collection)
 end
 
 
-
- def my_collect(collection)
-
-     x = 0
-    new_arr = []
-    while x < collection.length
-        new_arr << yield(collection[x])
-        x += 1
-    end
-    new_arr
-end
