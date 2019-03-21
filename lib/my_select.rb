4a6c9a8e99c57128f13406fe232780241bcb2f72
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-21 10:53:36 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,4 +2,16 @@ def my_select(collection)
  # your code here!
 end
 
-
+new_arr = []
+if collection.length > 0
+  while i < collection.length
+    if yield(collection[i])
+      new_arr << collection[i]
+    end
+    i+=1
+  end
+  else
+    puts "Empty collection"
+  end
+  new_arr
+end
